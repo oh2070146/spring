4fee4398e0a1776fcf1d93aa07fd86a67a14318b
oh jeong sik <oh jeong sik@DESKTOP-J5BQS2N>
2018-10-25 04:29:35 +0900

    hello this code spring project

---
+++ GroupWear/src/main/java/com/java/groupwear/mapper/BoardMapper.java
@@ -0,0 +1,25 @@
+package com.java.groupwear.mapper;
+
+import java.util.List;
+
+import com.java.groupwear.domain.BoardVO;
+import com.java.groupwear.domain.Criteria;
+
+public interface BoardMapper {
+
+	public List<BoardVO> getList();
+
+	public List<BoardVO> getListWithPaging(Criteria cri);
+
+	public void insert(BoardVO board);
+
+	public Integer insertSelectKey(BoardVO board);
+
+	public BoardVO read(Long bno);
+
+	public int delete(Long bno);
+
+	public int update(BoardVO board);
+
+	public int getTotalCount(Criteria cri);
+}
